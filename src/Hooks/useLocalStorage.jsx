4be88ad41e9ddd591a06406da4e121a933288e76
Wakil Azeez <wakilabdulazeez@gmail.com>
2023-09-04 21:19:59 +0100

    Adjust the local storage

--- src/Hooks/useLocalStorage.jsx
+++ src/Hooks/useLocalStorage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const useLocalStorage = ({innitialState}) => {
+const useLocalStorage = (innitialState) => {
   const [lists, setLists] = useState(() => {
     const storageItem = localStorage.getItem("item");
 
